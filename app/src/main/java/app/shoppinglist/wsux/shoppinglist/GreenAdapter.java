d8a31cb1d63b63543dde1a756e691f7223cf8183
Abuhay Abune <abuhay.abune@cs.huji.ac.il>
2018-12-01 15:21:43 +0200

    recycle view and adapter of list of numbers

---
+++ app/src/main/java/app/shoppinglist/wsux/shoppinglist/GreenAdapter.java
@@ -0,0 +1,68 @@
+
+package app.shoppinglist.wsux.shoppinglist;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHolder>
+{
+
+    private static final String TAG = GreenAdapter.class.getSimpleName();
+
+    private int mNumberItems;
+
+    public GreenAdapter(int numberOfItems) {
+        mNumberItems = numberOfItems;
+    }
+
+    @Override
+    public NumberViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.number_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        NumberViewHolder viewHolder = new NumberViewHolder(view);
+
+        return viewHolder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(NumberViewHolder holder, int position) {
+        Log.d(TAG, "#" + position);
+        holder.bind(position);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mNumberItems;
+    }
+
+
+    class NumberViewHolder extends RecyclerView.ViewHolder
+    {
+
+        // Will display the position in the list, ie 0 through getItemCount() - 1
+        TextView listItemNumberView;
+
+        public NumberViewHolder(View itemView) {
+            super(itemView);
+
+            listItemNumberView = (TextView) itemView.findViewById(R.id.tv_item_number);
+        }
+
+
+        void bind(int listIndex) {
+            listItemNumberView.setText(String.valueOf(listIndex));
+        }
+    }
+}
