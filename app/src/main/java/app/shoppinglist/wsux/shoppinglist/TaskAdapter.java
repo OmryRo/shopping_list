6078034588c8d3bd48388b836918aeae8166e234
Abuhay Abune <abuhay.abune@cs.huji.ac.il>
2018-12-07 12:35:21 +0200

    fix task adapter

--- app/src/main/java/app/shoppinglist/wsux/shoppinglist/TaskAdapter.java
+++ app/src/main/java/app/shoppinglist/wsux/shoppinglist/TaskAdapter.java
@@ -6,9 +6,12 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ItemViewHolder> {
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
 
     private static final String TAG = TaskAdapter.class.getSimpleName();
 
@@ -20,45 +23,49 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ItemViewHolder
     }
 
     @Override
-    public TaskAdapter.ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public TaskAdapter.TaskViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        TextView view = (TextView) LayoutInflater.from(parent.getContext())
+        LinearLayout view = (LinearLayout) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.task_list_item, parent, false);
 
-        ItemViewHolder viewHolder = new ItemViewHolder(view);
+        TaskViewHolder viewHolder = new TaskViewHolder(view);
 
         return viewHolder;
     }
 
 
     @Override
-    public void onBindViewHolder(ItemViewHolder holder, int position) {
-        Log.d(TAG, "#" + position);
-        holder.listItemNumberView.setText(mDataset[position]);
+    public void onBindViewHolder(TaskViewHolder holder, int position) {
+        holder.updateView(position);
     }
 
 
     @Override
     public int getItemCount() {
-        Log.d(TAG, "#" );
-        return mDataset.length;
+        return 100;
     }
 
 
-    class ItemViewHolder extends RecyclerView.ViewHolder {
+    class TaskViewHolder extends RecyclerView.ViewHolder {
 
-        // Will display the position in the list, ie 0 through getItemCount() - 1
-        public TextView listItemNumberView;
+        private LinearLayout listItemNumberView;
+        private TextView taskNameTv;
+        private TextView taskNoteTv;
+        private CheckBox statusCb;
+        private ImageView thumbnailIv;
 
-        public ItemViewHolder(TextView itemView) {
+        private TaskViewHolder(LinearLayout itemView) {
             super(itemView);
-
             listItemNumberView = itemView;
+            taskNameTv = itemView.findViewById(R.id.task_name_tv);
+            taskNoteTv = itemView.findViewById(R.id.task_note_tv);
+            statusCb = itemView.findViewById(R.id.task_status);
+            thumbnailIv = itemView.findViewById(R.id.task_thumbnail);
         }
 
-
-        void bind(int listIndex) {
-            listItemNumberView.setText(String.valueOf(listIndex));
+        private void updateView(int position) {
+            taskNameTv.setText(String.valueOf(position));
         }
+
     }
 }
