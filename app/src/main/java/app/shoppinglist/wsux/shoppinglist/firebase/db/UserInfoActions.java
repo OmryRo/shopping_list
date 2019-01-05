8fdfeb2003a769857fa43f9af5bebed59c88f525
abuhay.abune <T2c5k_l8sB>
2019-01-05 19:50:10 +0200

    add UserinfoActions.java file

---
+++ app/src/main/java/app/shoppinglist/wsux/shoppinglist/firebase/db/UserInfoActions.java
@@ -0,0 +1,40 @@
+package app.shoppinglist.wsux.shoppinglist.firebase.db;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class UserInfoActions {
+    public static final String FIRESTORE_TABLE = "users";
+    public static final String FIRESTORE_FIELD_LISTS = "lists";
+    public static final String FIRESTORE_FIELD_TOKENS = "tokens";
+    public static final String FIRESTORE_FIELD_LAST_LIST = "last_list";
+
+    public static DocumentReference getRef(FirebaseFirestore db, String userId) {
+        return db.collection(FIRESTORE_TABLE).document(userId);
+    }
+
+    public static TransactionWrapper addToken(
+            TransactionWrapper transaction,DocumentReference ref, String listId, String token){
+        return transaction.addToMap(ref, FIRESTORE_FIELD_TOKENS, listId, token);
+    }
+
+    public static TransactionWrapper addKnownList(
+            TransactionWrapper transaction,DocumentReference ref, String listId){
+        return transaction.addToList(ref,FIRESTORE_FIELD_LISTS, listId);
+    }
+
+    public static TransactionWrapper removeKnownList(
+            TransactionWrapper transaction,DocumentReference ref, String listId){
+        return transaction.removeFromList(ref,FIRESTORE_FIELD_LISTS, listId);
+    }
+
+    public static TransactionWrapper removeToken(
+            TransactionWrapper transaction,DocumentReference ref, String listId){
+        return transaction.removeFromMap(ref,FIRESTORE_FIELD_TOKENS, listId);
+    }
+
+    public static TransactionWrapper setLastList(
+            TransactionWrapper transaction,DocumentReference ref, String lastList){
+        return transaction.update(ref,FIRESTORE_FIELD_LAST_LIST, lastList);
+    }
+}
